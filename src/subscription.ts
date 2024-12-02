abc4b41a3acdbe2862fade0b6b13970727820645
ligengk <412552911@qq.com>
2024-12-02 22:16:41 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
-  checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  id: 6688,
+  name: 'Ligengk的GKD订阅',
+  version: 1.0,
+  author: 'ligengk',
+  checkUpdateUrl: './ligengk_gkd.version.json5',
+  supportUri: 'https://github.com/ligengk/gkd/issues/new/choose',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
