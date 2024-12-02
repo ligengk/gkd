a3090c2f1cbcdbf911ea5a656c4fbb9889bee4de
ligengk <412552911@qq.com>
2024-12-02 22:40:43 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -2,6 +2,21 @@ import { defineGkdSubscription } from '@gkd-kit/define';
 import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
 import globalGroups from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+import { OPEN_AD_ORDER } from './globalGroups';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (!g.name.startsWith('开屏广告')) {
+      g.enable = false;
+    } else {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
   id: 6688,
@@ -12,5 +27,5 @@ export default defineGkdSubscription({
   supportUri: 'https://github.com/ligengk/gkd/issues/new/choose',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps:rawApps,
 });
